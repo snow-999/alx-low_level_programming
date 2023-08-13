21067df42f2e8002a435ef52b2d6ea6c4cc3e2f6
khaled <kahledleader012@gmail.com>
2023-08-13 13:51:12 -0700

    msg

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main -entry point
+ *
+ * Description: print compo
+ *
+ * Return: always 0
+ *
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 0);
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
