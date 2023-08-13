b7a7a2a99a88603e3760b5fe9386965a0bbd3194
khaled <kahledleader012@gmail.com>
2023-08-13 11:51:05 -0700

    msg

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main -entery point
+ *
+ * Description: print ascii set
+ *
+ * Return: always 0
+*/
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(digit);
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
