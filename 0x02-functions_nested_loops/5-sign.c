317a9f4a5fa22246b58346289f3d865a20bcef59
khaled <kahledleader012@gmail.com>
2023-08-15 16:50:02 -0700

    msg

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - entry point
+ *
+ * Descriptin: print numbers statment
+ *
+ * Return: always 0
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
