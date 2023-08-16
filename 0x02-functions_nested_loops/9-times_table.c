a3a0a4827f2ef07279d9f8554b33ee38701e308a
khaled <kahledleader012@gmail.com>
2023-08-15 18:34:31 -0700

    msg

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * Times_table - entry point
+ *
+ * Description: print numbers
+*/
+
+void times_table(void)
+{
+	int l, n, n_2;
+
+	for (l = 0; l <= 9; l++)
+	{
+		_putchar(0);
+		for (n = 1; n <= 9; n++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			n_2 = l * n;
+			if (n_2 <= 9)
+			{
+				_putchar(' ');
+			} else
+				{
+					_putchar((n_2 / 10 + 48));
+					_putchar((n_2 % 10 + 48));
+				}
+			_putchar('\n');
+		}
+	}
+}
