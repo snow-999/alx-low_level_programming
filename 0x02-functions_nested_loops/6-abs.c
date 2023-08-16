90dc6005a50eeef735f4e278a22b739d68d1e52b
khaled <kahledleader012@gmail.com>
2023-08-15 17:24:20 -0700

    msg

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - entry point
+ *
+ * Description: print the abslute value
+ *
+ * Return: always 0
+ *
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+		return (n);
+	}
+	return (0);
+}
