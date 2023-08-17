6e0598eff1890b82a6f7263a4b8b4d86cecc8e93
khaled <kahledleader012@gmail.com>
2023-08-17 11:12:00 -0700

    msg

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/***/
+void more_numbers(void)
+{
+	int r, n, c;
+
+	for (r = 1; r <= 10; r++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			n = c;
+			if (c > 9)
+			{
+				_putchar(1 + 48);
+				n = c % 10;
+			}
+		_putchar(n + 48);
+		}
+		_putchar('\n');
+	}
+}
