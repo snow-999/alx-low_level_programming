dd6f909979487334ec7ad19bd9ebdc832cf104bb
khaled <kahledleader012@gmail.com>
2023-08-17 07:57:31 -0700

    msg

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - check uppercase letters
+ * @c: Task number
+ * Return: always 0
+ *
+*/
+int _isupper(int c)
+{
+	if (c == 65 && c <= 90)
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+		return (0);
+}
