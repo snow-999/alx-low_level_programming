5ae5bbddc35687fb03e7a4192169988760d4676d
khaled <kahledleader012@gmail.com>
2023-08-17 09:20:54 -0700

    msg

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print number
+ *
+ * Description: print numbers
+ *
+ * Return: null
+ *
+*/
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n + 48);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,9 +3,10 @@
 #include <stdio.h>
 
 int _isupper(int c);
+int _putchar(char c);
 int _isdigit(int c);
 int mul(int a, int b);
-
+void print_numbers(void);
 
 
 
