e4facf6439cb10da1cf3510b651a6b68666bbf56
khaled <kahledleader012@gmail.com>
2023-08-30 17:04:35 -0700

    mag

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - recusion session
+ * @s: task string
+ * Return: i
+ *
+*/
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	continue;
+	}
+	return (i);
+}
