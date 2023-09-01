ace5ec6efae4b49ed0002a7bb46c4823429fead7
khaled <kahledleader012@gmail.com>
2023-09-01 06:57:18 -0700

    mag

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -c *.c
-ar rc libball.a *.o
+gcc -Wall -Wextra -Werror -pedantic -c *.c
+ar -rc libball.a *.o
+ranlib libbal.a
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,4 @@
-#ifndef MANIN_H
+#ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 
