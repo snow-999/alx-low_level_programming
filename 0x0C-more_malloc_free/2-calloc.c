42e800366fddc2e9c808f3f40ff8c07efb77f12b
khaled <kahledleader012@gmail.com>
2023-09-07 12:17:13 -0700

    msg

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _alloc - make the string = 0
+ * @s: string
+ * @c: charater
+ * @n: size of array
+ * Return: str
+*/
+char *_alloc(char *s, char c, unsigned int n)
+{
+	char *str = s;
+
+	while (n--)
+	{
+		*s++ = c;
+	}
+	return (str);
+}
+/**
+ * _calloc - make 0 string
+ * @nmemb: size
+ * @size: size
+ * Return: ptr
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+	return (NULL);
+	}
+	ptr = malloc(sizeof(int) * nmemb);
+	if (ptr == NULL)
+	{
+	return (NULL);
+	}
+	_alloc(ptr, 0, sizeof(int) * nmemb);
+	return (ptr);
+}
