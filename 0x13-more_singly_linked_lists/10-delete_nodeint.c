8f0125ad419ad7e29db088f8d0dd5ccc8cdcc520
khaled <khaledleader012@gmail.com>
2023-09-26 01:14:22 -0700

    m

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *new, *old;
 	unsigned int x = 0;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 	{
 	return (-1);
 	}
