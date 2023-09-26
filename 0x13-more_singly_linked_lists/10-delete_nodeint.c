a6c7784df48698e9d28bd193d508183a0549beb2
khaled <khaledleader012@gmail.com>
2023-09-26 01:11:45 -0700

    m

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 	return (-1);
 	}
-	if (index == 0)
+	if (!index)
 	{
 	new = *head;
 	*head = (*head)->next;
@@ -34,5 +34,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		old = new;
 		new = new->next;
 	}
-	return (1);
+	return (-1);
 }
