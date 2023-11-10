1d52225306333d9e65ae6a8874aebcc93ee1ccfe
khaled <khaledleader012@gmail.com>
2023-11-10 08:24:13 -0800

    m

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-/***/
+/**
+ * dlistint_len - get the length of node
+ * @h: head node
+ * Return: number of elements
+ *
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t x = 0;
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add node at the begginig
+ * @head: the first node
+ * @n: data of the node
+ * Return: the addres of node
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	if (!new || !head)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->prev = NULL;
+	if (!*head)
+	{
+		*head = new;
+		new->next = NULL;
+	}
+	else
+	{
+		new->next = *head;
+		(*head)->prev = new;
+		*head = new;
+	}
+	return (new);
+}
