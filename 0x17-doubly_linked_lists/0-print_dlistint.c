af61d711f24dfb563fba8375787ab45bd1ceaeda
khaled <khaledleader012@gmail.com>
2023-11-10 07:54:25 -0800

    m

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - print elements of dlist
+ * @h: head node
+ * Return: number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
