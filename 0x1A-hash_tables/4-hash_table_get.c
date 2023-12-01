5e1cca0846f5cee4a75d37a8bca5f09e1a7964c7
khaled <khaledleader012@gmail.com>
2023-12-01 15:27:45 -0800

    m

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - get the value of key
+ * @ht: hash table node
+ * @key: the key will be given to acces the vlaue
+ * Return: the value of key
+ *
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int indx;
+	hash_node_t *current;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+			key == NULL || strlen(key) == 0)
+	{
+		return (NULL);
+	}
+	indx = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[indx];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->key);
+		}
+		current = current->next;
+	}
+	return (NULL);
+}
