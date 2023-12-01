643597d158fff8667ad632d7fc747ff34cdd5b52
khaled <khaledleader012@gmail.com>
2023-12-01 15:01:18 -0800

    m

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/***/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	unsigned long int x;
+
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+	ht->size = size;
+	ht->array = malloc(size * sizeof(hash_table_t *));
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	for (x = 0; x < size; x++)
+	{
+		ht->array[x] = NULL;
+	}
+	return (ht);
+}
